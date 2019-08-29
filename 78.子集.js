bc6e83f389bc4d494af85e30e8ff4581f330ac9f
zhuanghaixin <649308293@qq.com>
2019-08-29 23:54:12 +0800

    array | backtracking | bit-manipulation有点没想明白。。。

--- 78.子集.js
+++ 78.子集.js
@@ -7,6 +7,35 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+
+/*解决方法一
 var subsets = arr => arr.reduce((a, v) => a.concat(a.map(r => [v].concat(r))), [
   []
 ]);
+*/
+
+
+var subsets = function powerSet(str) {
+  var obj = {}
+  //这个循环是取出所有重复的数字/字母
+  for (var i = 0; i < str.length; i++) {
+    obj[str[i]] = true;
+  }
+  //这时变量数组没有重复项
+  var array = Object.keys(obj);
+  var result = [
+    []
+  ];
+  for (var i = 0; i < array.length; i++) {
+    var len = result.length;
+    for (var x = 0; x < len; x++) {
+      result.push(result[x].concat(array[i]))
+    }
+  }
+  return result;
+}
+
+/*
+
+用回溯算法，目前还不会
+*/
